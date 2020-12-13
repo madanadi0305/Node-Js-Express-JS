09d7b8fba57c49b1646933f0e57e8b3542bf5946
Adi Madan <madanadi0305@gmail.com>
2020-12-13 10:17:00 +0530

    Commited

--- src/express-application-mvc/app.js
+++ src/express-application-mvc/app.js
@@ -4,7 +4,7 @@ const app=express();
 app.use("/",routes);
 
 app.listen(5000,()=>{
-console.log("Application started listening on port : ",5000);
+console.log("Application started listening on port : ",6000);
 
 });
 module.exports=app;
